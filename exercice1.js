2226f7394904c8722c16d462fb345cd8c4a14ae0
ShyroVO <bosquet.laurie@outlook.com>
2020-12-04 09:42:03 +0100

    1.0

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,14 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+let monDiv2 = document.createElement('div');
 
 document.getElementById('viewport').appendChild(monDiv);
+document.getElementById('viewport').appendChild(monDiv2);
 
+monDiv.innerHTML="Ce div a été ajouté en utilisant javascript";
 
+monDiv2.innerHTML="Ce div a été ajouté en utilisant javascript";
+monDiv2.style.backgroundColor= "red";
+monDiv2.style.width="100px";
+monDiv2.style.height="100px";
 
-
-
-
-
-
+document.getElementById("aSupprimer").remove();
